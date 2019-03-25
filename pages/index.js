527fdb2a5add2f4ea34118ac79487b74b938fc1b
Orlando Del Aguila <orlando@hashlabs.com>
2019-03-25 12:23:34 -0600

    chore: setting up base

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const HelloWorld = styled.p`
+  color: red;
+`;
+
+export default () => (
+  <div>
+    <HelloWorld>Hello World!</HelloWorld>
+  </div>
+);
