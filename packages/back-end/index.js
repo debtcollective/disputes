a7b3fffad2f98ffb814a3ecdebca2bfdbc4eae71
Orlando Del Aguila <orlando@hashlabs.com>
2019-04-18 21:46:38 -0500

    feat: monorepo with now deployment

--- packages/back-end/index.js
+++ packages/back-end/index.js
@@ -1,8 +1,6 @@
-const { ApolloServer, gql } = require("apollo-server");
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
 const books = [
   {
     title: "Harry Potter and the Chamber of Secrets",
@@ -14,8 +12,6 @@ const books = [
   }
 ];
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
   # Comments in GraphQL are defined with the hash (#) symbol.
 
@@ -32,21 +28,28 @@ const typeDefs = gql`
   }
 `;
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
     books: () => books
   }
 };
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
-
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
 });
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+app.listen({ port }, () => {
+  console.log(
+    `🚀  Server ready at http://localhost:${port}${server.graphqlPath}`
+  );
+});
+
+export default app;
