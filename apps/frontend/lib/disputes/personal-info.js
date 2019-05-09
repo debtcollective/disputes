f707662e23f71096cc065f92bb6640c3404d64a7
Alexis Duran <alexis.duranm@gmail.com>
2019-05-09 09:00:08 +0200

    feat(disputes): add personal info lib file to be shared across form templates

---
+++ apps/frontend/lib/disputes/personal-info.js
@@ -0,0 +1,44 @@
+export default {
+  address: {
+    type: "string",
+    title: "Your Mailing Address"
+  },
+  birthday: {
+    type: "string",
+    title: "Date of Birth",
+    format: "date"
+  },
+  city: {
+    type: "string",
+    title: "Your City"
+  },
+  email: {
+    type: "string",
+    title: "Email"
+  },
+  name: {
+    type: "string",
+    title: "Your Full Name"
+  },
+  ssn: {
+    type: "string",
+    title: "SSN"
+  },
+  state: {
+    title: "Your State",
+    $ref: "#/definitions/usa-states"
+  },
+  telephone: {
+    type: "number",
+    title: "Your telephone"
+  },
+  telephoneAlt: {
+    type: "number",
+    title: "Your telephone (alt.)"
+  },
+  "zip-code": {
+    title: "Your Zip Code",
+    type: "string",
+    pattern: "[0-9]{5}"
+  }
+};
