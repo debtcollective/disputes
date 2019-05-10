d880c7fb7e88bde7f937daedafa99a53e3625e5b
Orlando Del Aguila <orlando@hashlabs.com>
2019-05-10 18:43:19 -0500

    chore: make material-ui work along styled-components

--- apps/frontend/pages/_app.js
+++ apps/frontend/pages/_app.js
@@ -1,6 +1,10 @@
 import { ApolloProvider } from "react-apollo";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import getPageContext from "../lib/getPageContext";
+import JssProvider from "react-jss/lib/JssProvider";
 import MomentUtils from "@date-io/moment";
 import { MuiPickersUtilsProvider } from "material-ui-pickers";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import { Normalize } from "styled-normalize";
 import React from "react";
 import withApolloClient from "../lib/with-apollo-client";
@@ -8,6 +12,19 @@ import App, { Container } from "next/app";
 
 // next.js custom App - https://nextjs.org/docs/#custom-app
 class MyApp extends App {
+  constructor() {
+    super();
+    this.pageContext = getPageContext();
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
   render() {
     const { Component, pageProps, apolloClient } = this.props;
 
@@ -16,9 +33,20 @@ class MyApp extends App {
         <Normalize />
         <Container>
           <ApolloProvider client={apolloClient}>
-            <MuiPickersUtilsProvider utils={MomentUtils}>
-              <Component {...pageProps} />
-            </MuiPickersUtilsProvider>
+            <JssProvider
+              registry={this.pageContext.sheetsRegistry}
+              generateClassName={this.pageContext.generateClassName}
+            >
+              <MuiPickersUtilsProvider utils={MomentUtils}>
+                <MuiThemeProvider
+                  theme={this.pageContext.theme}
+                  sheetsManager={this.pageContext.sheetsManager}
+                >
+                  <CssBaseline />
+                  <Component pageContext={this.pageContext} {...pageProps} />
+                </MuiThemeProvider>
+              </MuiPickersUtilsProvider>
+            </JssProvider>
           </ApolloProvider>
         </Container>
       </React.Fragment>
--- apps/frontend/pages/index.js
+++ apps/frontend/pages/index.js
@@ -1,6 +1,7 @@
 import Header from "@debtcollective/header";
 import Link from "next/link";
 import React from "react";
+import styled from "styled-components";
 
 const links = [
   { href: "/admin/disputes", roles: ["admin"], text: "Admin" },
@@ -9,11 +10,17 @@ const links = [
   { href: "https://community.debtcollective.org", text: "Community" },
 ];
 
+const Title = styled.h1`
+  color: red;
+  font-size: 50px;
+`;
+
 const Page = () => {
   return (
     <React.Fragment>
       <Header links={links} />
       <div>
+        <Title>Working</Title>
         <Link href="/disputes/general">
           <a>General Dispute</a>
         </Link>
