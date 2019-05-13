4e57809d150e5a1ca6f2fc09cdea540e4c658af8
Alexis Duran <alexis.duranm@gmail.com>
2019-05-13 09:04:40 +0200

    refactor(taxes): create a shared schema file to be used on all taxes types

---
+++ apps/frontend/lib/disputes/taxes-offset-shared.js
@@ -0,0 +1,175 @@
+import debtTypes from "./dispute-types";
+import personalInfo from "./personal-info";
+import usaStates from "./usa-states";
+
+const schemas = {
+  json: {
+    $schema: "http://json-schema.org/schema#",
+    definitions: {
+      "debt-types": {
+        enum: debtTypes.values,
+        enumNames: debtTypes.labels,
+        type: "string",
+      },
+      "usa-states": {
+        enum: usaStates.values,
+        enumNames: usaStates.labels,
+        type: "string",
+      },
+    },
+    properties: {
+      FFELLoan: {
+        dependencies: {
+          ffelHolder: {
+            oneOf: [
+              {
+                properties: {
+                  ffelHolder: {
+                    enum: [false],
+                  },
+                },
+              },
+              {
+                properties: {
+                  ffelHolder: {
+                    enum: [true],
+                  },
+                  guarantyAgency: {
+                    properties: {
+                      address: {
+                        title: "Address",
+                        type: "string",
+                      },
+                      city: {
+                        title: "City",
+                        type: "string",
+                      },
+                      name: {
+                        title: "Name",
+                        type: "string",
+                      },
+                      state: {
+                        $ref: "#/definitions/usa-states",
+                        title: "State",
+                      },
+                      "zip-code": {
+                        pattern: "[0-9]{5}",
+                        title: "Zip Code",
+                        type: "string",
+                      },
+                    },
+                    required: ["address", "city", "name", "state", "zip-code"],
+                    title: "Guaranty Agency details",
+                    type: "object",
+                  },
+                },
+                required: ["ffelHolder", "guarantyAgency"],
+              },
+            ],
+          },
+        },
+        properties: {
+          ffelHolder: {
+            description:
+              "If you have a FFEL loan, add the name and address of your guaranty agency in the box below. The name and address may appear on the tax offset notice you received in the mail. If you don't know the name and address of your guarantor, you can contact the Department of Education or call 1-800-304-3107 and ask for this information.",
+            title: "Are you a FFEL holder?",
+            type: "boolean",
+          },
+        },
+        title: "FFEL Loan",
+        type: "object",
+      },
+      personalInformation: {
+        description: "Let’s get started, tell us about you",
+        properties: {
+          ...personalInfo,
+        },
+        required: [
+          "address",
+          "birthday",
+          "city",
+          "email",
+          "name",
+          "ssn",
+          "state",
+          "telephone",
+          "telephoneAlt",
+          "zip-code",
+        ],
+        title: "Personal Information",
+        type: "object",
+      },
+      yourSchool: {
+        description:
+          "Information related to the school where you incurred the debt",
+        properties: {
+          address: {
+            title: "Address",
+            type: "string",
+          },
+          attend: {
+            properties: {
+              from: {
+                format: "date",
+                title: "from",
+                type: "string",
+              },
+              to: {
+                format: "date",
+                title: "to",
+                type: "string",
+              },
+            },
+            required: ["from", "to"],
+            title: "When did you attend the school?",
+            type: "object",
+          },
+          city: {
+            title: "City",
+            type: "string",
+          },
+          name: {
+            title: "Name",
+            type: "string",
+          },
+          state: {
+            $ref: "#/definitions/usa-states",
+            title: "State",
+          },
+          "zip-code": {
+            pattern: "[0-9]{5}",
+            title: "Your Zip Code",
+            type: "string",
+          },
+        },
+        required: ["address", "attend", "city", "name", "state", "zip-code"],
+        title: "Your School",
+        type: "object",
+      },
+    },
+    title: "Dispute Your Tax Return Being Taken",
+    type: "object",
+  },
+  ui: {
+    FFELLoan: { "ui:order": ["*"] },
+    personalInformation: {
+      "ui:order": [
+        "name",
+        "ssn",
+        "address",
+        "city",
+        "state",
+        "zip-code",
+        "birthday",
+        "email",
+        "*",
+      ],
+    },
+    "ui:order": ["personalInformation", "yourSchool", "FFELLoan", "*"],
+    yourSchool: {
+      "ui:order": ["name", "address", "city", "state", "zip-code", "*"],
+    },
+  },
+};
+
+export default schemas;
--- apps/frontend/pages/disputes/tax/c.js
+++ apps/frontend/pages/disputes/tax/c.js
@@ -1,87 +1,13 @@
-import debtTypes from "../../../lib/disputes/dispute-types";
 import FieldTemplate from "../../../components/FieldTemplate";
 import Form from "react-jsonschema-form";
-import personalInfo from "../../../lib/disputes/personal-info";
 import React from "react";
-import usaStates from "../../../lib/disputes/usa-states";
+import sharedSchema from "../../../lib/disputes/taxes-offset-shared";
 
 const schemas = {
   json: {
-    $schema: "http://json-schema.org/schema#",
-    definitions: {
-      "debt-types": {
-        enum: debtTypes.values,
-        enumNames: debtTypes.labels,
-        type: "string",
-      },
-      "usa-states": {
-        enum: usaStates.values,
-        enumNames: usaStates.labels,
-        type: "string",
-      },
-    },
+    ...sharedSchema.json,
     properties: {
-      FFELLoan: {
-        dependencies: {
-          ffelHolder: {
-            oneOf: [
-              {
-                properties: {
-                  ffelHolder: {
-                    enum: [false],
-                  },
-                },
-              },
-              {
-                properties: {
-                  ffelHolder: {
-                    enum: [true],
-                  },
-                  guarantyAgency: {
-                    properties: {
-                      address: {
-                        title: "Address",
-                        type: "string",
-                      },
-                      city: {
-                        title: "City",
-                        type: "string",
-                      },
-                      name: {
-                        title: "Name",
-                        type: "string",
-                      },
-                      state: {
-                        $ref: "#/definitions/usa-states",
-                        title: "State",
-                      },
-                      "zip-code": {
-                        pattern: "[0-9]{5}",
-                        title: "Zip Code",
-                        type: "string",
-                      },
-                    },
-                    required: ["address", "city", "name", "state", "zip-code"],
-                    title: "Guaranty Agency details",
-                    type: "object",
-                  },
-                },
-                required: ["ffelHolder", "guarantyAgency"],
-              },
-            ],
-          },
-        },
-        properties: {
-          ffelHolder: {
-            description:
-              "If you have a FFEL loan, add the name and address of your guaranty agency in the box below. The name and address may appear on the tax offset notice you received in the mail. If you don't know the name and address of your guarantor, you can contact the Department of Education or call 1-800-304-3107 and ask for this information.",
-            title: "Are you a FFEL holder?",
-            type: "boolean",
-          },
-        },
-        title: "FFEL Loan",
-        type: "object",
-      },
+      ...sharedSchema.json.properties,
       atb: {
         properties: {
           "applying-as-parent": {
@@ -131,7 +57,7 @@ const schemas = {
           "attend-time": {
             title:
               "Did you (or the student) successfully complete 6 credit hours or 225 clock hours of coursework that applied toward a program offered by the school before you received a Direct or a FFEL loan?",
-            type: "string",
+            type: "boolean",
           },
           "attend-when-enroll": {
             format: "date",
@@ -160,96 +86,10 @@ const schemas = {
         title: "False Certification - Ability to Benefit",
         type: "object",
       },
-      personalInformation: {
-        description: "Let’s get started, tell us about you",
-        properties: {
-          ...personalInfo,
-        },
-        required: [
-          "address",
-          "birthday",
-          "city",
-          "email",
-          "name",
-          "ssn",
-          "state",
-          "telephone",
-          "telephoneAlt",
-          "zip-code",
-        ],
-        title: "Personal Information",
-        type: "object",
-      },
-      yourSchool: {
-        description:
-          "Information related to the school where you incurred the debt",
-        properties: {
-          address: {
-            title: "Address",
-            type: "string",
-          },
-          attend: {
-            properties: {
-              from: {
-                format: "date",
-                title: "from",
-                type: "string",
-              },
-              to: {
-                format: "date",
-                title: "to",
-                type: "string",
-              },
-            },
-            required: ["from", "to"],
-            title: "When did you attend the school?",
-            type: "object",
-          },
-          city: {
-            title: "City",
-            type: "string",
-          },
-          name: {
-            title: "Name",
-            type: "string",
-          },
-          state: {
-            $ref: "#/definitions/usa-states",
-            title: "State",
-          },
-          "zip-code": {
-            pattern: "[0-9]{5}",
-            title: "Your Zip Code",
-            type: "string",
-          },
-        },
-        required: ["address", "attend", "city", "name", "state", "zip-code"],
-        title: "Your School",
-        type: "object",
-      },
     },
-    title: "Dispute Your Tax Return Being Taken",
-    type: "object",
   },
   ui: {
-    FFELLoan: { "ui:order": ["*"] },
-    personalInformation: {
-      "ui:order": [
-        "name",
-        "ssn",
-        "address",
-        "city",
-        "state",
-        "zip-code",
-        "birthday",
-        "email",
-        "*",
-      ],
-    },
-    "ui:order": ["personalInformation", "yourSchool", "FFELLoan", "*"],
-    yourSchool: {
-      "ui:order": ["name", "address", "city", "state", "zip-code", "*"],
-    },
+    ...sharedSchema.ui,
   },
 };
 
