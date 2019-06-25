d64fad06d5d29ea3020c00aba2e73a552fbfd69f
Alexis Duran <alexis.duranm@gmail.com>
2019-06-25 08:36:55 +0200

    feat(document-generation): add initial implementation of DocumentGenerator for Credit and General Disputes

--- apps/document-generation/src/documents/CreditReportDispute.js
+++ apps/document-generation/src/documents/CreditReportDispute.js
@@ -1,7 +1,17 @@
 // @flow
 
+import PDFEngine from "../engines/PDFEngine";
+
 class CreditReportDispute implements DocumentGenerator {
-  static generateFiles = (data: mixed) => data;
+  engine = PDFEngine;
+  generateFiles = (data: mixed) => {
+    console.log("generate files", data);
+
+    return [new File([], "fileName")];
+  };
+  slug = "credit-report-dispute";
+  templates = ["/general_debt_dispute_letter/0.hbs"];
+  version = "v1";
 }
 
-export default CreditReportDispute;
+export default new CreditReportDispute();
--- apps/document-generation/src/documents/GeneralDispute.js
+++ apps/document-generation/src/documents/GeneralDispute.js
@@ -2,8 +2,16 @@
 
 import PDFEngine from "../engines/PDFEngine";
 
-class CreditReportDispute implements DocumentGenerator {
+class GeneralDispute implements DocumentGenerator {
   engine = PDFEngine;
+  generateFiles = (data: mixed) => {
+    console.log("generate files", data);
+
+    return [new File([], "fileName")];
+  };
+  slug = "general-dispute";
+  templates = ["/general_debt_dispute_letter/0.hbs"];
+  version = "v1";
 }
 
-export default new CreditReportDispute();
+export default new GeneralDispute();
