57a5a30ef97ebe0799a2e263b93e04f291f73d08
Orlando Del Aguila <orlando@hashlabs.com>
2019-07-16 21:31:54 -0500

    refactor: move general-debt to tools

--- apps/frontend/lib/tools/index.js
+++ apps/frontend/lib/tools/index.js
@@ -6,5 +6,6 @@ export {
   default as PrivateStudentLoanForm,
   privateStudentLoanSchema,
 } from "./private-student-loan";
+export { default as GeneralDebtForm, generalDebtSchema } from "./general-debt";
 export * from "./tax-offset";
 export * from "./wage-garnishment";
